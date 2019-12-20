c9f860309fc3cd7213e0cd38027966f1c4eae552
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-20 15:48:45 -0600

    styling cart page for small devices

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Img from 'react-image';
 import batcombeLord from '../images/products/batcombe-lord.jpg';
+import { ReactComponent as SvgTrashCan } from '../icons/trash-can.svg';
 
 const Cart = () => {
   return (
     <>
-      <section className='w-full bg-gray-200 py-10 px-4'>
+      <section className='w-full bg-gray-200 pt-12 px-4'>
         <h2 className='text-xl font-bold uppercase tracking-wide mb-4'>
           your shopping bag{' '}
           <span className='font-light text-sm text-gray-800 ml-2'>(2)</span>
@@ -25,8 +26,8 @@ const Cart = () => {
                   <h3 className='w-full uppercase font-medium tracking-wide'>
                     batcombe lord
                   </h3>
-                  <button className='border p-2 border-gray-300 rounded-sm'>
-                    delete
+                  <button className='border py-2 px-4 border-gray-300 rounded-sm'>
+                    <SvgTrashCan className='w-4 h-4'></SvgTrashCan>
                   </button>
                 </div>
               </div>
@@ -39,11 +40,46 @@ const Cart = () => {
               Color: Noir
             </span>
           </div>
-          <div className='py-6 px-4'>amount & price</div>
+          <div className='flex flex-wrap items-center justify-between py-6 px-4'>
+            <div className='border border-black'>
+              <button className='bg-gray-100 px-4 py-2'>-</button>
+              <input className='w-12 px-4 py-2' type='text' value='1' />
+              <button className='bg-gray-100 px-4 py-2'>+</button>
+            </div>
+
+            <span className='font-medium'>$ 1,680.00</span>
+          </div>
         </article>
-        <article className='w-full bg-white rounded-sm mb-12'>
-          <div className='py-6 px-4 border-b border-gray-300'>
-            subtotal & total
+        <article className='w-full bg-white rounded-sm'>
+          <div className='flex flex-wrap items-center justify-between py-6 px-4 border-b border-gray-300 tracking-wide leading-loose'>
+            <div className='w-1/2 text-left capitalize mb-4'>subtotal :</div>
+            <div className='w-1/2 text-right mb-4'>$ 2,470.00</div>
+            <div className='w-1/2 text-left capitalize mb-4'>shipping :</div>
+            <div className='w-1/2 text-right mb-4'>$0.00</div>
+            <div className='w-1/2 text-left font-bold capitalize text-xl'>
+              total :
+            </div>
+            <div className='w-1/2 text-right font-bold text-xl'>2, 470.00</div>
+          </div>
+        </article>
+      </section>
+      <section className='w-full bg-gray-200 py-12 px-4'>
+        <h2 className='text-xl font-bold uppercase tracking-wide mb-4'>
+          shopping details
+        </h2>
+        <article className='w-full bg-white rounded-sm'>
+          <div className=' py-6 px-4 border-b border-gray-300 tracking-wide leading-loose'>
+            <div className='w-full text-left text-xs capitalize mb-4'>
+              email *
+            </div>
+            <input className='w-full mb-4 border border-gray-300 rounded-sm' />
+            <div className='w-full text-left text-xs capitalize mb-4'>
+              credit card *
+            </div>
+            <input className='w-full mb-4 border border-gray-300 rounded-sm' />
+            <button className='w-full bg-black tracking-wider font-light text-xs text-white text-center py-3 rounded-sm mt-10 mb-6'>
+              Pay with Credit Card
+            </button>
           </div>
         </article>
       </section>
