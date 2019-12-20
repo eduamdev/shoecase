3a431a72d834e89df718c6df7dce66f6dd72851e
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-20 16:22:24 -0600

    darken footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,47 +6,53 @@ import { ReactComponent as SvgInstagram } from '../icons/instagram.svg';
 const Footer = () => {
   return (
     <footer className='border-t border-gray-300'>
-      <section className='py-6'>
+      <section className='bg-black py-6'>
         <article className='w-full p-6'>
-          <p className='capitalize font-medium mb-4'>
+          <p className='capitalize font-medium mb-4 text-gray-300'>
             Sign Up to Our Newsletter
           </p>
           <input
-            className='w-full px-4 py-2 bg-gray-200 border-none outline-none text-sm tracking-wider'
+            className='w-full px-4 py-2 bg-gray-900 text-gray-400 border-none outline-none text-sm tracking-wider'
             placeholder='ENTER YOUR EMAIL'
             type='text'
           />
         </article>
         <article className='inline-block align-top w-1/2 p-6'>
-          <p className='capitalize font-medium font-sm mb-4'>store</p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+            store
+          </p>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             contact us
           </p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             our store
           </p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             about us
           </p>
         </article>
         <article className='inline-block align-top w-1/2 p-6'>
-          <p className='capitalize font-medium font-sm mb-4'>orders</p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+            orders
+          </p>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             delivery & returns
           </p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             my account
           </p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
             collect in store
           </p>
-          <p className='text-sm tracking-wide leading-loose text-gray-800 uppercase leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 uppercase leading-loose'>
             faq
           </p>
         </article>
         <article className='w-full p-6'>
-          <p className='capitalize font-medium font-sm mb-4'>social</p>
-          <div className='flex mb-4'>
+          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+            social
+          </p>
+          <div className='flex mb-4 text-gray-400 fill-current'>
             <SvgFacebook className='h-4 w-4 mr-6'></SvgFacebook>
             <SvgTwitter className='h-4 w-4 mr-6'></SvgTwitter>
             <SvgInstagram className='h-4 w-4'></SvgInstagram>
