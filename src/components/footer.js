62938fd61cfe60166e3ca8ee6c1d55bf783ea7c6
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-21 15:28:09 -0600

    footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,51 +5,54 @@ import { ReactComponent as SvgInstagram } from '../icons/instagram.svg';
 
 const Footer = () => {
   return (
-    <footer className='border-t border-gray-300'>
-      <section className='flex flex-wrap bg-black py-6 lg:px-10 xl:px-24 lg:py-10'>
-        <article className='w-full md:w-1/4 p-6'>
-          <p className='capitalize font-medium mb-4 text-gray-300'>
+    <footer className='bg-black border-t border-gray-300'>
+      <section
+        className='flex flex-wrap py-6 lg:px-12 md:justify-between md:py-16 xl:py-24 lg:text-lg mx-auto'
+        style={{ maxWidth: '1440px' }}
+      >
+        <article className='w-full md:w-1/4 lg:w-2/6 p-6'>
+          <p className='capitalize font-medium mb-4 md:mb-6 text-gray-300'>
             Sign Up to Our Newsletter
           </p>
           <input
-            className='w-full px-4 py-2 bg-gray-900 text-gray-400 border-none outline-none text-sm tracking-wider'
+            className='w-full px-4 py-3 md:px-6 md:py-4 bg-gray-900 text-gray-400 border-none outline-none text-sm tracking-wider rounded-sm'
             placeholder='ENTER YOUR EMAIL'
             type='text'
           />
         </article>
-        <article className='inline-block align-top w-1/2 md:w-1/4 p-6'>
-          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+        <article className='inline-block align-top w-1/2 md:w-1/4 lg:w-1/6 p-6'>
+          <p className='capitalize font-medium font-sm mb-4 md:mb-6 text-gray-300'>
             store
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             contact us
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             our store
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             about us
           </p>
         </article>
-        <article className='inline-block align-top w-1/2 md:w-1/4 p-6'>
-          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+        <article className='inline-block align-top w-1/2 md:w-1/4 lg:w-1/6 p-6'>
+          <p className='capitalize font-medium font-sm mb-4 md:mb-6 text-gray-300'>
             orders
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             delivery & returns
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             my account
           </p>
-          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose'>
+          <p className='text-sm tracking-wider font-light leading-loose text-gray-400 capitalize leading-loose lg:text-base'>
             collect in store
           </p>
           <p className='text-sm tracking-wider font-light leading-loose text-gray-400 uppercase leading-loose'>
             faq
           </p>
         </article>
-        <article className='w-full md:w-1/4 p-6'>
-          <p className='capitalize font-medium font-sm mb-4 text-gray-300'>
+        <article className='w-full md:w-1/4 lg:w-1/6 p-6'>
+          <p className='capitalize font-medium font-sm mb-4 md:mb-6 text-gray-300'>
             social
           </p>
           <div className='flex mb-4 text-gray-400 fill-current'>
