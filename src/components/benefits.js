6ff70a79fee2870defa84e3c5869d0f3ef073cfc
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-21 14:51:52 -0600

    responsive changes

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -8,42 +8,42 @@ import customizable from '../images/customizable.jpg';
 const Benefits = () => {
   return (
     <section className='flex flex-wrap'>
-      <div className='w-full md:w-1/3 md:border-r border-b border-gray-300 p-6'>
+      <div className='w-full md:w-1/3 md:border-r border-b border-gray-300 p-6 md:p-12'>
         <Img
-          className='w-full h-40 object-cover rounded-sm'
+          className='w-full h-40 md:h-56 xl:h-64 object-cover rounded-sm'
           src={comfort}
           alt='A pair of comfortable shoes'
         />
-        <p className='pt-4 font-bold uppercase leading-loose'>comfort</p>
-        <p className=' text-sm tracking-wide'>Like walking on the clouds</p>
+        <p className='pt-4 font-bold uppercase leading-loose lg:text-2xl lg:tracking-wide'>comfort</p>
+        <p className=' text-sm tracking-wide lg:text-base font-light'>Like walking on the clouds</p>
       </div>
-      <div className='w-full md:w-1/3 md:border-r border-b border-gray-300 p-6'>
+      <div className='w-full md:w-1/3 md:border-r border-b border-gray-300 p-6 md:p-12'>
         <Img
-          className='w-full h-40 object-cover rounded-sm'
+          className='w-full h-40 md:h-56 xl:h-64 object-cover rounded-sm'
           src={waterproof}
           alt='A pair of waterproof shoes'
         />
-        <p className='pt-4 font-bold uppercase leading-loose'>waterproof</p>
-        <p className=' text-sm tracking-wide'>No more wet socks</p>
+        <p className='pt-4 font-bold uppercase leading-loose lg:text-2xl lg:tracking-wide'>waterproof</p>
+        <p className=' text-sm tracking-wide lg:text-base font-light'>No more wet socks</p>
       </div>
-      <div className='w-full md:w-1/3 border-b border-gray-300 p-6'>
+      <div className='w-full md:w-1/3 border-b border-gray-300 p-6 md:p-12'>
         <Img
-          className='w-full h-40 object-cover rounded-sm'
+          className='w-full h-40 md:h-56 xl:h-64 object-cover rounded-sm'
           src={lightWeight}
           alt='very light weight sneakers'
         />
-        <p className='pt-4 font-bold uppercase leading-loose'>light weight</p>
-        <p className=' text-sm tracking-wide'>As light as air</p>
+        <p className='pt-4 font-bold uppercase leading-loose lg:text-2xl lg:tracking-wide'>light weight</p>
+        <p className=' text-sm tracking-wide lg:text-base font-light'>As light as air</p>
       </div>
-      <div className='w-full p-6 pb-24 md:pb-6'>
+      <div className='relative w-full lg:w-5/6 p-6 pb-24 md:p-12'>
         <Img
-          className='w-full h-56 md:h-108 md:pr-40 object-cover relative rounded-sm'
+          className='w-full h-72 md:h-108 md:pr-40 lg:h-auto lg:p-0 object-cover relative rounded-sm'
           src={customizable}
           alt='A rack of shoes'
         />
-        <div className='absolute left-0 right-0 -mt-8 mx-auto w-64 md:w-72 bg-white p-6 md:right-0 md:left-auto md:-mt-32 md:mr-10 rounded-sm shadow-md'>
-          <p className='font-bold uppercase leading-loose'>customizable</p>
-          <p className=' text-sm tracking-wide'>Your shoes, your style</p>
+        <div className='absolute left-0 right-0 -mt-8 mx-auto w-64 md:w-72 lg:w-84 md:right-0 md:left-auto md:-mt-32 md:bottom-0 md:mb-24 md:p-10 lg:-mr-16 bg-white p-6 rounded-sm shadow-md'>
+          <p className='font-bold uppercase leading-loose lg:text-2xl lg:tracking-wide'>customizable</p>
+          <p className=' text-sm tracking-wide lg:text-base font-light'>Your shoes, your style</p>
         </div>
       </div>
     </section>
