95cf42d5619fd0bea9ce9938e99901ab59b9b937
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-28 03:34:35 -0600

    font size

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -8,10 +8,10 @@ const ProductCard = ({ url, slug, alt, name, price }) => {
       <Link to={url}>
         <div className='h-full w-full border-4 xl:border-8 border-transparent hover:border-gray-200 p-6 lg:p-12'>
           <Image slug={slug} alt={alt} />
-          <span className='block mt-4 lg:mt-0 md:pt-8 md:pt-10 font-medium  uppercase md:text-base xl:text-lg tracking-wide'>
+          <span className='block mt-4 lg:mt-0 md:pt-8 md:pt-10 font-medium uppercase text-sm md:text-base xl:text-lg tracking-wide'>
             {name}
           </span>
-          <span className='pb-4 font-light tracking-wide text-base xl:text-lg leading-loose'>
+          <span className='pb-4 font-light tracking-wide text-sm md:text-base xl:text-lg leading-loose'>
             {price}
           </span>
         </div>
