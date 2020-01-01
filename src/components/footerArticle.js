6d35b5285b8d437ae32ffc9d9703c112bc76247f
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-31 19:54:03 -0600

    few footer changes

--- src/components/footerArticle.js
+++ src/components/footerArticle.js
@@ -9,22 +9,22 @@ const articles = {
   default: () => null
 };
 
-const renderTitle = title => (
+const ArticleTitle = ({ children }) => (
   <h3 className='capitalize font-medium font-sm mb-4 md:mb-6 text-gray-300'>
-    {title}
+    {children}
   </h3>
 );
 
 const renderInput = ({ title, name, input }) => (
   <article className='w-full md:w-1/4 xl:w-2/6 px-4 md:px-6 lg:px-12 py-6 md:py-16 '>
-    {renderTitle(title)}
+    <ArticleTitle>{title}</ArticleTitle>
     <Input name={name} placeholder={input.placeholder} dark></Input>
   </article>
 );
 
 const renderCategories = ({ title, categories }) => (
   <article className='inline-block align-top w-1/2 md:w-1/4 xl:w-1/6 px-4 md:px-6 lg:px-12 py-6 md:py-16 '>
-    {renderTitle(title)}
+    <ArticleTitle>{title}</ArticleTitle>
     {categories &&
       categories.map(c => (
         <span
@@ -39,7 +39,7 @@ const renderCategories = ({ title, categories }) => (
 
 const renderIcons = ({ title, icons }) => (
   <article className='w-full md:w-1/4 xl:w-1/6 px-4 md:px-6 lg:px-12 py-6 md:py-16 '>
-    {renderTitle(title)}
+    <ArticleTitle>{title}</ArticleTitle>
     <div className='flex mb-4 text-gray-400 fill-current'>
       {icons &&
         icons.map(i => (
