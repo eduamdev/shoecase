b2917d86dca72aff63c8b7236e548da8637f4e15
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2020-01-03 03:13:31 -0600

    $$$

--- src/components/shoppingBagTotal.js
+++ src/components/shoppingBagTotal.js
@@ -4,7 +4,7 @@ const ShoppingBagTotal = ({ totalPrice }) => {
   return (
     <div className='flex flex-wrap justify-between items-center py-12 border-t border-gray-200'>
       <span className='font-bold text-xl uppercase'>total</span>
-      <span className='font-bold text-xl'>{totalPrice}</span>
+      <span className='font-bold text-xl'>$ {totalPrice}</span>
     </div>
   );
 };
