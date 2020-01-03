69f0b2531999499637bfacd8dc4943c815af863b
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2020-01-03 04:14:11 -0600

    add link to product in shopping bag

--- src/components/shoppingBagProduct.js
+++ src/components/shoppingBagProduct.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Image from './common/image';
 import { NumberUtils } from '../utils';
 
 const ShoppingBagProduct = ({ products }) => {
-  return products.map(product => (
-    <div
-      key={product._id}
-      className='flex flex-wrap justify-between items-center py-5 border-t border-b border-gray-200'
-    >
-      <Image name={product.slug} type='shoppingBag'></Image>
-      <span className='w-1/2 uppercase'>{product.name}</span>
-      <span className=''>${NumberUtils.formatCurrency(product.price, 2)}</span>
-    </div>
-  ));
+  return (
+    products &&
+    products.map(product => (
+      <Link key={product._id} to={`/products/${product.slug}`}>
+        <div className='flex flex-wrap justify-between items-center py-5 border-t border-b border-gray-200'>
+          <Image name={product.slug} type='shoppingBag'></Image>
+          <span className='w-1/2 uppercase'>{product.name}</span>
+          <span className=''>
+            ${NumberUtils.formatCurrency(product.price, 2)}
+          </span>
+        </div>
+      </Link>
+    ))
+  );
 };
 
 export default ShoppingBagProduct;
