a3b8e5a8f5817a69c9bb500ad9dad8ad121a6472
Edroamz <35645733+edroamz@users.noreply.github.com>
2022-04-13 15:57:37 -0500

    Updates

---
+++ precache-manifest.62caf8f95a9781df46f1808208976b99.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8525971461d00ee0ec55af86d13817e4",
+    "url": "/shoecase/index.html"
+  },
+  {
+    "revision": "c33fb121e1e2d5596646",
+    "url": "/shoecase/static/css/main.da5285b6.chunk.css"
+  },
+  {
+    "revision": "61044751db151001154a",
+    "url": "/shoecase/static/js/2.ba023d92.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shoecase/static/js/2.ba023d92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c33fb121e1e2d5596646",
+    "url": "/shoecase/static/js/main.33ab6f64.chunk.js"
+  },
+  {
+    "revision": "c2d3bc3f0099399a492c",
+    "url": "/shoecase/static/js/runtime-main.3c769afb.js"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/shoecase/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/shoecase/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/shoecase/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "8ea2657f263b6b2a404bfb57f4160013",
+    "url": "/shoecase/static/media/Poppins-ExtraLight.8ea2657f.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/shoecase/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "926a872e20b6555f4d2a395d6d1bc01e",
+    "url": "/shoecase/static/media/Poppins-LightItalic.926a872e.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/shoecase/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/shoecase/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/shoecase/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "4bd05c92f0f1b8120ce9787c53252744",
+    "url": "/shoecase/static/media/arrow-up.4bd05c92.svg"
+  },
+  {
+    "revision": "d500e524821b5470bba14f3f56b04899",
+    "url": "/shoecase/static/media/banner.d500e524.jpg"
+  },
+  {
+    "revision": "5065726e89dc16dda8dc9b3c06797a2b",
+    "url": "/shoecase/static/media/batcombe-lord.5065726e.jpg"
+  },
+  {
+    "revision": "1682716429319fdcfbcd5dfb76aa3ad1",
+    "url": "/shoecase/static/media/calla-rose.16827164.webp"
+  },
+  {
+    "revision": "c0e4ffa80114edc2939699d5c43d0389",
+    "url": "/shoecase/static/media/check-mark.c0e4ffa8.svg"
+  },
+  {
+    "revision": "e5e62321891d8f1145f9de716a75b60a",
+    "url": "/shoecase/static/media/close.e5e62321.svg"
+  },
+  {
+    "revision": "71073c46d5387dd25402d8fadf46469c",
+    "url": "/shoecase/static/media/comfort.71073c46.jpg"
+  },
+  {
+    "revision": "2f2a1044f610bb100cc5e7f11a67ae01",
+    "url": "/shoecase/static/media/customizable.2f2a1044.jpg"
+  },
+  {
+    "revision": "616417cd4111d2b6702a17181afa2ad9",
+    "url": "/shoecase/static/media/facebook.616417cd.svg"
+  },
+  {
+    "revision": "cf6783b2ffedeae992ef39870513161a",
+    "url": "/shoecase/static/media/filter.cf6783b2.svg"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/shoecase/static/media/instagram.341a67c5.svg"
+  },
+  {
+    "revision": "da991fdb55e95bbc6abae0ac1366436b",
+    "url": "/shoecase/static/media/light-weight.da991fdb.jpg"
+  },
+  {
+    "revision": "31f8e2600975976fc4410daeb041c2e4",
+    "url": "/shoecase/static/media/loading.31f8e260.svg"
+  },
+  {
+    "revision": "4c32f726bb4e6d555c4769d4bd6cdcf8",
+    "url": "/shoecase/static/media/magnifier.4c32f726.svg"
+  },
+  {
+    "revision": "00b27c6bbcdc5e3c924b155ef99e5b23",
+    "url": "/shoecase/static/media/menu.00b27c6b.svg"
+  },
+  {
+    "revision": "fa39ad957674c5c5c36bd755562786a8",
+    "url": "/shoecase/static/media/mila-myth.fa39ad95.webp"
+  },
+  {
+    "revision": "69b30251c6aa797f09b4e1938c3fd6dc",
+    "url": "/shoecase/static/media/minus.69b30251.svg"
+  },
+  {
+    "revision": "67bc60a9fc12edea2f53a0f08f89ccbb",
+    "url": "/shoecase/static/media/no-results.67bc60a9.jpg"
+  },
+  {
+    "revision": "9b08278346507cef320f9f9556af5c09",
+    "url": "/shoecase/static/media/plus.9b082783.svg"
+  },
+  {
+    "revision": "3231458c01f0398944859f7aab368219",
+    "url": "/shoecase/static/media/shopping-bag.3231458c.svg"
+  },
+  {
+    "revision": "d7bec4e6ace665bae3d9410540a4a0b2",
+    "url": "/shoecase/static/media/trash-can.d7bec4e6.svg"
+  },
+  {
+    "revision": "631cd5664eb1d4a27681a21ca4ad6126",
+    "url": "/shoecase/static/media/twitter.631cd566.svg"
+  },
+  {
+    "revision": "fd8bc9feb83bd952d72e1689653bc36d",
+    "url": "/shoecase/static/media/user.fd8bc9fe.svg"
+  },
+  {
+    "revision": "a489eabd16e85b5ec3aecdf80822946e",
+    "url": "/shoecase/static/media/waterproof.a489eabd.jpg"
+  }
+]);
